a2e6499f7ca142fb89bc7b5958f0fd5db635d56f
KuiMing <benjamin0901@gmail.com>
2024-09-22 23:28:14 +0800

    feat: combine RAG and streamlit
    1. Use Qdrant as vector store
    2. Let user enter collection name

--- streaming.py
+++ streaming.py
@@ -1,9 +1,16 @@
+from typing import List
 import streamlit as st
 from langchain_core.messages import AIMessage, HumanMessage
-from langchain_openai import ChatOpenAI, AzureChatOpenAI
-from dotenv import dotenv_values
-from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import AzureOpenAIEmbeddings, AzureChatOpenAI
+
+# pylint: disable=no-name-in-module
+from langchain import schema
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from qdrant_client import QdrantClient
+from langchain_qdrant import QdrantVectorStore
+from dotenv import dotenv_values
 
 
 config = dotenv_values(".env")
@@ -13,37 +20,66 @@ st.set_page_config(page_title="Streaming bot", page_icon="🤖")
 st.title("Streaming bot")
 
 
-def get_response(user_query, chat_history):
-
-    template = """
-    You are a helpful assistant. Answer the following questions considering the history of the conversation:
-
-    Chat history: {chat_history}
-
-    User question: {user_question}
+def get_response(
+    user_query: str, chat_history: List[schema.HumanMessage], collection_name: str
+):
     """
-
-    prompt = ChatPromptTemplate.from_template(template)
-
-    llm = AzureChatOpenAI(
+    Generates a response to the user's query based on the provided
+    chat history and a specified document collection.
+    Args:
+        user_query: The query from the user.
+        chat_history: The history of the chat as a list of HumanMessage objects.
+        collection_name: The name of the document collection to use for reference.
+    Returns:
+        generator: A generator that streams the response to the user's query.
+    Raises:
+        ValueError: If any required configuration is missing or invalid.
+    """
+    system_prompt = (
+        "你是一位專門根據文件回答問題的 AI 助手。如果你無法從文件得到答案，請說你不知道。"
+        "請根據以下參考資料回答問題："
+        "歷史紀錄：{chat_history}"
+        "參考資料：{context}"
+    )
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            ("human", "{input}"),
+        ]
+    )
+    generator_llm = AzureChatOpenAI(
         azure_endpoint=config.get("AZURE_OPENAI_ENDPOINT"),
         azure_deployment=config.get("AZURE_OPENAI_DEPLOYMENT_NAME"),
         openai_api_version=config.get("AZURE_OPENAI_API_VERSION"),
         api_key=config.get("AZURE_OPENAI_KEY"),
         streaming=True,
     )
-
-    chain = prompt | llm | StrOutputParser()
-
-    return chain.stream(
-        {
-            "chat_history": chat_history,
-            "user_question": user_query,
-        }
+    embedding_llm = AzureOpenAIEmbeddings(
+        azure_endpoint=config.get("AZURE_OPENAI_ENDPOINT"),
+        azure_deployment=config.get("AZURE_OPENAI_Embedding_DEPLOYMENT_NAME"),
+        api_key=config.get("AZURE_OPENAI_KEY"),
+        openai_api_version=config.get("AZURE_OPENAI_API_VERSION"),
     )
+    question_answer_chain = create_stuff_documents_chain(generator_llm, prompt)
+    client = QdrantClient(url="http://localhost:6333")
+    qdrant = QdrantVectorStore(
+        client=client, collection_name=collection_name, embedding=embedding_llm
+    )
+    retriever = qdrant.as_retriever(search_kwargs={"k": 3})
+
+    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+    chain = rag_chain.pick("answer")
+    return chain.stream({"input": user_query, "chat_history": chat_history})
 
 
 def main():
+    """
+    main function for the Streamlit app.
+    """
+
+    collection_name = st.sidebar.text_input(
+        "請輸入要查詢的 Collection 名稱", value="DefaultCollection"
+    )
 
     # session state
     if "chat_history" not in st.session_state:
@@ -70,7 +106,11 @@ def main():
 
         with st.chat_message("AI"):
             response = st.write_stream(
-                get_response(user_query, st.session_state.chat_history)
+                get_response(
+                    user_query=user_query,
+                    chat_history=st.session_state.chat_history,
+                    collection_name=collection_name,
+                )
             )
 
         st.session_state.chat_history.append(AIMessage(content=response))
